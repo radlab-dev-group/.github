455f1cd05ba6a6ad1b51da061aa62f2b589d876d
pawkedzia <pawel@radlab.dev>
2026-06-16 21:29:13 +0200

    Add full augmentation workflow to `03_examples_augemntation.sh`: define output directory and LLM router settings, convert raw Clarin‑PL TwitterEmo dataset to labeled JSONL, and invoke `genai-data-augmentation` with proper parameters.

--- conf/DSS_20260618/code/augmentation/03_examples_augemntation.sh
+++ conf/DSS_20260618/code/augmentation/03_examples_augemntation.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
-echo "Augemntowanie przykładów uczących"
+OUT_DIR=resources/dataset/twitteremo/augmented
+
+LLM_ROUTER_MODEL="gpt-oss:120b"
+LLM_ROUTER_HOST="http://192.168.100.65:8080"
+
+DATASET_JSONL_FILE="resources/dataset/twitteremo/clarinpl-twitteremo-train-sample-5k.jsonl"
+LABELED_DATASET_JSONL="resources/dataset/twitteremo/clarinpl-twitteremo-train-sample-5k_labels.jsonl"
+
+mkdir -p "${OUT_DIR}"
+
+python3 code/dataset/convert_raw_clarin_to_labels.py \
+  --input "${DATASET_JSONL_FILE}" \
+  --output "${LABELED_DATASET_JSONL}"
+
+genai-data-augmentation \
+  --dataset-path="${LABELED_DATASET_JSONL}" \
+  --labels="pozytywny" \
+  --output-dir="${OUT_DIR}" \
+  --prompt-file=resources/prompts/augomentator/augmentator.prompt \
+  --labels=pozytywny \
+  --llm-router-url="${LLM_ROUTER_HOST}" \
+  --model-name="${LLM_ROUTER_MODEL}" \
+  --temperature=0.0 \
+  --batch-save-size=2 \
+  --num-workers=2 \
+  --n-sample=0 \
+  --text-column-name="text" \
+  --label-column-name="labels"
