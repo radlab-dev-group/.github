4e648a68c435875eec3a434d010a0eddba5c1a39
pawkedzia <pawel@radlab.dev>
2026-06-16 11:10:44 +0200

    Add output‑directory creation and detailed `genai-classifier` options to `01_label_with_genai.sh`; introduce `polarity_classifier.prompt` defining JSON format for emotional polarity annotation.

--- conf/DSS_20260618/code/augmentation/01_label_with_genai.sh
+++ conf/DSS_20260618/code/augmentation/01_label_with_genai.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+OUT_DIR=resources/dataset/twitteremo/genailabelled
 
-echo "Wykorzystanie llm-router-utils genai-classifier"
+LLM_ROUTER_MODEL="gpt-oss:120b"
+LLM_ROUTER_HOST="http://192.168.100.65:8080"
+
+mkdir -p "${OUT_DIR}"
+
+genai-classifier \
+  --dataset-dir=resources/dataset/twitteremo/clarinpl-twitteremo-train-sample-5k.jsonl \
+  --prompts-dir=resources/prompts \
+  --output-dir=${OUT_DIR} \
+  --llm-router-url="${LLM_ROUTER_HOST}" \
+  --model-name="${LLM_ROUTER_MODEL}" \
+  --temperature=0.0 \
+  --batch-save-size=2 \
+  --num-workers=2 \
+  --n-sample=0
