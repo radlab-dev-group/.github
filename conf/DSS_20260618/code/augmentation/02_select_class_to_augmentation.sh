91fae9103a68dc156cd593e982681bd86fb7c7c9
pawkedzia <pawel@radlab.dev>
2026-06-16 21:21:02 +0200

    Add class distribution visualization call to the augmentation script.

--- conf/DSS_20260618/code/augmentation/02_select_class_to_augmentation.sh
+++ conf/DSS_20260618/code/augmentation/02_select_class_to_augmentation.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-echo "Ktora klasa po rozkladzie do augmentacji?"
-# Uruchamiamy class distribution na genailabelled
+python3 code/dataset/visualize_class_distribution.py \
+  --train resources/dataset/twitteremo/genailabelled/clarinpl-twitteremo-train-sample-5k_training.jsonl \
+  --valid resources/dataset/twitteremo/clarinpl-twitteremo-valid-sample-500.jsonl \
+  --output resources/dataset/twitteremo/genailabelled/clarinpl-twitteremo-train-valid-distribution.png
