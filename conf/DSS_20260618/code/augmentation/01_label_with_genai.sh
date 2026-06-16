604c3482e707e7f82c049cd3656ed6f86309ed75
pawkedzia <pawel@radlab.dev>
2026-06-16 11:35:27 +0200

    Add configurable `DATASET_DIR` variable and use it for `--dataset-dir`; introduce `--text-column-name="tekst"` option in `01_label_with_genai.sh`.

--- conf/DSS_20260618/code/augmentation/01_label_with_genai.sh
+++ conf/DSS_20260618/code/augmentation/01_label_with_genai.sh
@@ -4,11 +4,12 @@ OUT_DIR=resources/dataset/twitteremo/genailabelled
 
 LLM_ROUTER_MODEL="gpt-oss:120b"
 LLM_ROUTER_HOST="http://192.168.100.65:8080"
+DATASET_DIR="resources/dataset/twitteremo/"
 
 mkdir -p "${OUT_DIR}"
 
 genai-classifier \
-  --dataset-dir=resources/dataset/twitteremo/clarinpl-twitteremo-train-sample-5k.jsonl \
+  --dataset-dir="${DATASET_DIR}" \
   --prompts-dir=resources/prompts \
   --output-dir=${OUT_DIR} \
   --llm-router-url="${LLM_ROUTER_HOST}" \
@@ -16,4 +17,5 @@ genai-classifier \
   --temperature=0.0 \
   --batch-save-size=2 \
   --num-workers=2 \
-  --n-sample=0
+  --n-sample=0 \
+  --text-column-name="tekst"
