81bc50baa45995f8a426f1f7d394b870149baff5
pawkedzia <pawel@radlab.dev>
2026-06-17 09:39:43 +0200

    Enhance `07_web_app_dump_data_and_run_training.sh`: add output path variables, generate class‑distribution visualization, clean previous model directory, and launch training with `CUDA_VISIBLE_DEVICES` and updated hyper‑parameters.

--- conf/DSS_20260618/07_web_app_dump_data_and_run_training.sh
+++ conf/DSS_20260618/07_web_app_dump_data_and_run_training.sh
@@ -2,15 +2,37 @@
 
 export PYTHONPATH=$PYTHONPATH:.
 
-# Tworzymy zrzut danych z bazy decyzji usderów (nieoznaczone też wpadają do datasetu)
-# Uczymy model na zmergowanym datasecie
+OUT_TRAINING_FILE="resources/dataset/twitteremo/active_learning/twitter-emo-sample-5k-manual-1_7k-augmented_active_learning.jsonl"
+OUT_TRAINING_FILE_DISTR="resources/dataset/twitteremo/active_learning/twitter-emo-sample-5k-manual-1_7k-augmented_active_learning.png"
 
+# ----------------------------------------------------------------------------------------------------------------------
 # 1. Zrzut danych z bazy danych
+# Tworzymy zrzut danych z bazy decyzji usderów (nieoznaczone też wpadają do datasetu)
 python3 code/dataset/dump_dataset_from_web_app.py \
   --model "/mnt/local/models/dss-2026-06/polarity-model/twitter-emo-sample-5k-manual-1_7k-augmented" \
-  --output-path "resources/dataset/twitteremo/active_learning/twitter-emo-sample-5k-manual-1_7k-augmented_active_learning.jsonl"
+  --output-path "${OUT_TRAINING_FILE}"
 
-# 2. Uruchomienie treningu (miejsce na wywołanie skryptu trenującego)
-# Model wyuczony jest pod ściezką /mnt/local....
-# i dostępny będzie w aplikacji
-#  -> Podgląd na Confusion Matrix w wandb
+# ----------------------------------------------------------------------------------------------------------------------
+# 2. Przygotowujemy rozkład klas
+python3 code/dataset/visualize_class_distribution.py \
+  --train "${OUT_TRAINING_FILE}" \
+  --valid resources/dataset/twitteremo/clarinpl-twitteremo-valid-sample-500.jsonl \
+  --output "${OUT_TRAINING_FILE_DISTR}"
+
+# ----------------------------------------------------------------------------------------------------------------------
+# 3. Uruchomienie treningu (miejsce na wywołanie skryptu trenującego)
+# Uczymy model na danych ręczne + augmentowane + po activle learning (HIL)
+#  -> Oceniamy go na wydzielonym niezależnie samplu 500 przykładów
+#  -> Ocena tego modelu jest na tym samym valid-set co w pozostałych treningach
+
+OUT_DIR="/mnt/local/models/dss-2026-06/polarity-model/twitter-emo-sample-5k-manual-1_7k-augmented_active_learning"
+rm -rf "${OUT_DIR}"
+CUDA_VISIBLE_DEVICES=1 python3 code/training/train_polarity_model.py \
+  --train "${OUT_TRAINING_FILE}" \
+  --valid resources/dataset/twitteremo/clarinpl-twitteremo-valid-sample-500.jsonl \
+  --base-model-path allegro/herbert-base-cased \
+  --num-epochs 5 \
+  --batch-size 32 \
+  --learning-rate 3e-6 \
+  --wandb-project polar-twitteremo \
+  --output-dir "${OUT_DIR}"
