301624cfc3cbb2bbd1fde8ec2b7f586304e5c49b
pawkedzia <pawel@radlab.dev>
2026-06-17 08:34:59 +0200

    Add scripts to prepare and merge augmented data; update training script comments to reflect training on manual + augmented dataset.

--- conf/DSS_20260618/04_train_base_manual_with_aug_1_7k.sh
+++ conf/DSS_20260618/04_train_base_manual_with_aug_1_7k.sh
@@ -4,6 +4,10 @@ OUT_DIR="/mnt/local/models/dss-2026-06/polarity-model/twitter-emo-sample-5k-manu
 
 rm -rf "${OUT_DIR}"
 
+# Uczymy model na danych ręczne + augmentowane
+#  -> Oceniamy go na wydzielonym niezależnie samplu 500 przykładów
+#  -> Ocena tego modelu i modelu uczonego na podstawie ręcznych anotacji jest na tym samym valid-set
+
 CUDA_VISIBLE_DEVICES=1 python3 code/training/train_polarity_model.py \
   --train resources/dataset/twitteremo/merged/clarinpl-twitteremo-train-sample-5k-and-augmented-pos-1_7k.jsonl \
   --valid resources/dataset/twitteremo/clarinpl-twitteremo-valid-sample-500.jsonl \
