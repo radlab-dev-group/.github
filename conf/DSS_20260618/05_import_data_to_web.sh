18c7f49e63169eb79202c43d99cd24dee0abce4b
pawkedzia <pawel@radlab.dev>
2026-06-17 08:13:10 +0200

    Update import script to use augmented TwitterEmo training dataset.

--- conf/DSS_20260618/05_import_data_to_web.sh
+++ conf/DSS_20260618/05_import_data_to_web.sh
@@ -1,4 +1,4 @@
  #!/bin/bash
 
 bash code/web_app/import_to_db.sh \
-  resources/dataset/twitteremo/genailabelled/clarinpl-twitteremo-train-sample-5k_training.jsonl
+  resources/dataset/twitteremo/augmented/clarinpl-twitteremo-train-sample-5k_labels_augmented-training.jsonl
