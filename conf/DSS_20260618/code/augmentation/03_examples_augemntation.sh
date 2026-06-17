6edb78429a80b5294fc972f4a58f15d559001bbb
pawkedzia <pawel@radlab.dev>
2026-06-17 07:12:00 +0200

    Refactor augmentation script: define separate merged/augmented output directories, merge augmented data with original dataset, and add class‑distribution visualization step.

--- conf/DSS_20260618/code/augmentation/03_examples_augemntation.sh
+++ conf/DSS_20260618/code/augmentation/03_examples_augemntation.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-OUT_DIR=resources/dataset/twitteremo/augmented
+DATA_DIR=resources/dataset/twitteremo
+
+OUT_DIR_MERGED="${DATA_DIR}/merged"
+OUT_DIR_AUGMENTED="${DATA_DIR}/augmented"
 
 LLM_ROUTER_MODEL="gpt-oss:120b"
 LLM_ROUTER_HOST="http://192.168.100.65:8080"
 
-DATASET_JSONL_FILE="resources/dataset/twitteremo/clarinpl-twitteremo-train-sample-5k.jsonl"
-LABELED_DATASET_JSONL="resources/dataset/twitteremo/clarinpl-twitteremo-train-sample-5k_labels.jsonl"
+DATASET_JSONL_FILE="${DATA_DIR}/clarinpl-twitteremo-train-sample-5k.jsonl"
+LABELED_DATASET_JSONL="${DATA_DIR}/clarinpl-twitteremo-train-sample-5k_labels.jsonl"
+OUT_MERGED_PNG="${OUT_DIR_MERGED}/clarinpl-twitteremo-train-sample-5k-and-augmented-pos-1_7k.png"
+OUT_MERGED_JSONL="${OUT_DIR_MERGED}/clarinpl-twitteremo-train-sample-5k-and-augmented-pos-1_7k.jsonl"
 
-mkdir -p "${OUT_DIR}"
+mkdir -p "${OUT_DIR_MERGED}"
+mkdir -p "${OUT_DIR_AUGMENTED}"
 
+# Konwersja do formatu akceptowalnego przez genai-data-augmentation
 python3 code/dataset/convert_raw_clarin_to_labels.py \
   --input "${DATASET_JSONL_FILE}" \
   --output "${LABELED_DATASET_JSONL}"
 
+# Urchomienie genai-data-augmentation
 genai-data-augmentation \
   --dataset-path="${LABELED_DATASET_JSONL}" \
   --labels="pozytywny" \
-  --output-dir="${OUT_DIR}" \
+  --output-dir="${OUT_DIR_AUGMENTED}" \
   --prompt-file=resources/prompts/augomentator/augmentator.prompt \
   --labels=pozytywny \
   --llm-router-url="${LLM_ROUTER_HOST}" \
@@ -31,6 +39,19 @@ genai-data-augmentation \
   --text-column-name="text" \
   --label-column-name="labels"
 
+# Konwersja genai-data-augmentation do formatu takiego samego
+# jak clarinpl-twitteremo-train-sample-5k.jsonl -> aby wykorzystać ten sam skrypt uczący
 python3 code/dataset/convert_genai_to_training.py \
-  --input "${OUT_DIR}/clarinpl-twitteremo-train-sample-5k_labels_augmented-train.jsonl" \
-  --output "${OUT_DIR}/clarinpl-twitteremo-train-sample-5k_labels_augmented-training.jsonl"
+  --input "${OUT_DIR_AUGMENTED}/clarinpl-twitteremo-train-sample-5k_labels_augmented-train.jsonl" \
+  --output "${OUT_DIR_AUGMENTED}/clarinpl-twitteremo-train-sample-5k_labels_augmented-training.jsonl"
+
+# Dołączanie zbioru augmentowanego do ręcznego
+cat \
+  "${OUT_DIR_AUGMENTED}/clarinpl-twitteremo-train-sample-5k_labels_augmented-training.jsonl" \
+  "${DATA_DIR}/clarinpl-twitteremo-train-sample-5k.jsonl" | sed 's/"text"/"tekst"/g' | shuf > "${OUT_MERGED_JSONL}"
+
+# Rozkład klas po łączeniu
+python3 code/dataset/visualize_class_distribution.py \
+  --train "${OUT_MERGED_JSONL}" \
+  --valid resources/dataset/twitteremo/clarinpl-twitteremo-valid-sample-500.jsonl \
+  --output "${OUT_MERGED_PNG}"
